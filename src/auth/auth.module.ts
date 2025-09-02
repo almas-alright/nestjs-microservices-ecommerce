be7445d82e5e512c60f1209e437820b6d3145ec8
almas-techmons <kutsnalmas@techmonstar.com>
2025-09-02 12:15:32 +0600

    config spreaded

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '../config/config.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -6,8 +7,21 @@ import { UserModule } from '../user/user.module';
 import { JwtService } from './jwt.service';
 
 @Module({
-  imports: [UserModule, JwtModule.register({ secret: 'your-secret-key' })],
-  providers: [AuthService, JwtService],
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET_KEY'), // Fetch JWT secret from auth.env
+        // signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+  ],
+  providers: [
+    { provide: ConfigService, useFactory: () => new ConfigService('auth') },
+    AuthService,
+    JwtService,
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
